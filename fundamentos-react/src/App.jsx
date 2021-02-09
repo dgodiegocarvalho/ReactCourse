c2dda13a8cad2813f7f9fb0788c1a974b8337ea1
Diego <dgo.diegocarvalho@gmail.com>
2021-02-08 21:47:40 -0300

    Aula de cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 
-//import Card from "./components/layout/Card";
+import Card from "./components/layout/Card";
 
 import Aleatorio from "./components/basicos/Aleatorio";
 import Fragmento from "./components/basicos/Fragmento";
@@ -12,13 +12,26 @@ export default () => (
     <div className="App">
         <h1>Fundamentos React</h1>
 
-        <Aleatorio min={1} max={60} />
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={9.3}
-        />
-        <Primeiro></Primeiro>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório" color="#070">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#260126">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com Parametro" color="#59323C">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro" color="#F2EEB3">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
 )
