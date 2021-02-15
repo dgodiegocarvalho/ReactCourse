e9f02ae9157f31caa754a22e0451cbc954ff80b6
Diego <dgo.diegocarvalho@gmail.com>
2021-02-15 17:35:11 -0300

    Contador, aula componente baseado em classe

--- fundamentos-react/src/components/contador/PassoForm.jsx
+++ fundamentos-react/src/components/contador/PassoForm.jsx
@@ -5,7 +5,11 @@ export default props => {
     return (
         <div>
             <label htmlFor="passoinput">Passo: </label>
-            <input id="passoInput" type="number" value={props.passo} onChange={e => props.setPasso(+e.target.value)} />
+            <input 
+                id="passoInput" 
+                type="number" 
+                value={props.passo} 
+                onChange={e => props.setPasso(+e.target.value)} />
         </div>
     )
 }
